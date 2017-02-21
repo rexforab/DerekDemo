43bf71fbc5c88387859097512dd17a917a605cf3
Master <Master@MASTER-PC>
2017-02-20 20:40:21 -0500

    Publish settings and entity migrations

---
+++ DerekDemo/DerekDemo.Service/Migrations/201702201705502_InitialCreate.cs
@@ -0,0 +1,42 @@
+namespace DerekDemo.Service.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Invoices",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Amount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        DueDate = c.DateTime(nullable: false),
+                        User_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Users", t => t.User_Id)
+                .Index(t => t.User_Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Invoices", "User_Id", "dbo.Users");
+            DropIndex("dbo.Invoices", new[] { "User_Id" });
+            DropTable("dbo.Users");
+            DropTable("dbo.Invoices");
+        }
+    }
+}
